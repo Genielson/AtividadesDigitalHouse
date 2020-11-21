6ceff8559aaef2b05b3854bc3c63633c407f0c64
Genielson <ge.nielson@hotmail.com>
2020-11-21 07:22:30 -0300

    Adicionando a class NetworkUtils

---
+++ android/ExercicioAula35/app/src/main/java/com/example/exerciciochucknorris/data/NetworkUtils.kt
@@ -0,0 +1,17 @@
+package com.example.exerciciochucknorris.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkUtils {
+
+    companion object{
+        
+        fun getRetrofitInstance(baseUrl:String): Retrofit {
+
+            return Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build()
+
+        }
+
+    }
+}
